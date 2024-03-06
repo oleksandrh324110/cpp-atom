a0f2aec285f096012f9b66c8002451fc5937a634
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-03-06 21:31:43 +0200

    O(n) sorted array search

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <array>
+#include <cmath>
 
+struct MaxAndMin {
+  int max, min;
+};
 
+template<typename T, size_t size>
+MaxAndMin findMaxAndMin(std::array<T, size> arr) {
+  MaxAndMin res = { arr[0], arr[0] };
+
+  for (size_t i = 1; i < arr.size(); i++) {
+    res.max = std::max(res.max, arr[i]);
+    res.min = std::min(res.min, arr[i]);
+  }
+
+  return res;
+}
 
 int main(void) {
-  typedef char custom_type;
+  std::array<int, 10> arr = { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 
-  custom_type n = -1;
+  MaxAndMin res = findMaxAndMin(arr);
 
-  for (
-    custom_type i = 0, mask = (custom_type)(1 << (sizeof(custom_type) * 8 - 1));
-    i < (custom_type)sizeof(custom_type) * 8;
-    mask = (custom_type)1 << (sizeof(custom_type) * 8 - 1 - ++i)
-    ) {
-    if (i && i % 4 == 0)
-      putchar(' ');
-    putchar(mask & n ? '1' : '0');
-  } putchar('\n');
+  std::cout << "max in arr: " << res.max << "\nmin in arr: " << res.min << '\n';
 }
